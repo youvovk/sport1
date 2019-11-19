3b3c1336a6a1fe880b93f73f40ad588d3759f215
youvovk <yuriy3vovk@gmail.com>
2019-11-19 17:50:22 +0200

    Updates

---
+++ precache-manifest.b23af0d9d9274dcf2fd611a2e4248cb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a61350c2aa0870a7ec443156c3f64fd",
+    "url": "/sport1/index.html"
+  },
+  {
+    "revision": "dd09779ced0a4ea00f0e",
+    "url": "/sport1/static/css/main.1db4e849.chunk.css"
+  },
+  {
+    "revision": "c7d89afed3d0fe2aff47",
+    "url": "/sport1/static/js/2.a137866a.chunk.js"
+  },
+  {
+    "revision": "dd09779ced0a4ea00f0e",
+    "url": "/sport1/static/js/main.7bc27fc4.chunk.js"
+  },
+  {
+    "revision": "ec305e5e8ed2782d9862",
+    "url": "/sport1/static/js/runtime-main.3ccc0099.js"
+  },
+  {
+    "revision": "c50f996cdca238cb34223d658193ed86",
+    "url": "/sport1/static/media/back-footer.c50f996c.jpg"
+  },
+  {
+    "revision": "035b0af3d3b649ef846bf32726a42c85",
+    "url": "/sport1/static/media/back-main.035b0af3.jpg"
+  },
+  {
+    "revision": "80a3a1b8cfc6e55e9678f835d344b07c",
+    "url": "/sport1/static/media/back-text.80a3a1b8.jpg"
+  }
+]);
